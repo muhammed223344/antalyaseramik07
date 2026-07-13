f64fe653ee8eece5158503eceaeeef4e1bae0935
muhammed223344 <saadsaad750183@gmail.com>
2026-07-13 09:37:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -326,3 +326,23 @@ clearInterval(interval);
 },500);
 
 }
+/* ===========================
+   LIVE CLOCK
+=========================== */
+
+function updateClock(){
+
+const now = new Date();
+
+const h = String(now.getHours()).padStart(2,"0");
+const m = String(now.getMinutes()).padStart(2,"0");
+const s = String(now.getSeconds()).padStart(2,"0");
+
+document.getElementById("live-clock").textContent =
+h + ":" + m + ":" + s;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
